0b4f4c15d603261a755f43785ccaa7d9e982fa70
fish <41461914+zhiyu1997@users.noreply.github.com>
2022-05-09 16:56:13 +0800

    Update Observer_Method.cpp

--- Observer_Method.cpp
+++ Observer_Method.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 // 观察者模式
+/**
+ * @description: 
+ * @param {int} argc
+ * @param {一个对象的状态发生改变时，
+ * 所有依赖于它的对象都得到通知并被自动更新。
+ * 如微博中一个明星发了一个消息，所有关注的用户都会看到这条消息。
+ * @return {*}
+ */
 int main(int argc, char **argv){
 
     View *view1 = new TableView("TableView1");
